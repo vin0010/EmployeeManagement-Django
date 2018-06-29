f0a55d772c33b243af77f79e2aec83b15f90adce
Vinoth G <vin10catchme@gmail.com>
2018-06-29 15:42:03 +0530

    Added models for student

--- src/student_manager/models.py
+++ src/student_manager/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     father_name = models.CharField(max_length=60)
     age = models.SmallIntegerField
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    address = models.TextField
+    id = models.AutoField
+    std = models.SmallIntegerField
+    remarks = models.TextField
+
+    def __str__(self):
+        return self.title
