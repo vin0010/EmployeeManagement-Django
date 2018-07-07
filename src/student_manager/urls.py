2e53318ffbd445589333974c11a230a8c357b8d2
Vinoth G <vin10catchme@gmail.com>
2018-07-07 10:16:04 +0530

    Model for students

--- src/student_manager/urls.py
+++ src/student_manager/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 
 from .views import (
                 get_student,
-                get_all_student
+                get_all_student,
+                create_student
                 )
 
 urlpatterns = [
     url(r'(?P<id>\d+)/$', get_student, name="get_student"),
     url(r'all/$', get_all_student, name='all'),
+    url(r'create/$', create_student)
 ]
--- src/student_manager/views.py
+++ src/student_manager/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Student
 
-
+from .forms import StudentForm
 # Create your views here.
 
 def get_student(request, id=0):
@@ -13,6 +13,14 @@ def get_student(request, id=0):
     return render(request, "view_student.html", context)
     # return HttpResponse("<h1>Welcome Student<h1>")
 
+
+def create_student(request):
+    form = StudentForm()
+    context = {
+        "form":form
+    }
+    # return HttpResponse("<h1>Welcome Student Form<h1>")
+    return render(request, "create_student.html", context)
 
 def get_all_student(request):
     students = Student.objects.all()
