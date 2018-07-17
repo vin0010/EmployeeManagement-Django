9a5eca6e9f804059fa1fefc96f89f9648e9b3bf0
gjergjk71 <gjergjk71@gmail.com>
2018-07-17 16:34:45 +0200

    Added all fields to StudentForm

--- src/student_manager/forms.py
+++ src/student_manager/forms.py
@@ -2,10 +2,7 @@ from django import forms
 from .models import Student
 
 class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = [
-            "first_name",
-            "last_name",
-            "age"
-        ]
+	class Meta:
+		model = Student
+		#fields generates a list of all editable fields of Student model
+		fields = [f.name for f in Student._meta.get_fields() if not f.name in ["created","modified"]]
